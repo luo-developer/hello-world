051254203e1bf5f4286e4a6179f648bbbee033e9
luo-developer <m15975226835@163.com>
2020-08-20 11:46:15 +0800

    全删除了

--- ac696n_soundbox_sdk_v0.2.5/SDK/apps/common/third_party_profile/include/JL_rcsp_adv/rcsp_adv_setting/adv_mic_setting.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ADV_MIC_SETTING_H__
-#define __ADV_MIC_SETTING_H__
-
-void set_mic_setting(u8 mic_setting_info);
-u8 get_mic_setting(void);
-
-#endif
--- ac696n_soundbox_sdk_v0.2.5/SDK/apps/soundbox/include/music/music.h
+++
@@ -1,5 +0,0 @@
-#ifndef _MUSIC_H_
-#define _MUSIC_H_
-#include "music/music_player.h"
-
-#endif
